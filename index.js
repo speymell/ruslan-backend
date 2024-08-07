9ed69a06be1bde5a84fb04c2cef7ce3d862d1f1b
Slava <slava@mail.ru>
2024-08-07 16:24:36 +0700

    version6

--- index.js
+++ index.js
@@ -26,24 +26,8 @@ mongoose
 
 const app = express();
 app.use(express.json());
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.status(200).send();
-});
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.post("/auth/login", loginValidation, UserController.login);
 
 app.post("/auth/register", registerValidation, UserController.register);
