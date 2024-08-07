6cd318ff90a981f587aad847a3aec5c9ddca81aa
Slava <slava@mail.ru>
2024-08-07 13:25:48 +0700

    version6

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.post("/auth/login", loginValidation, UserController.login);
 
 app.post("/auth/register", registerValidation, UserController.register);
